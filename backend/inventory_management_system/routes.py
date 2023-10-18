52abaa085459ea8216fbd87f98b76b2073891c2c
PickolZi <pickol876@gmail.com>
2023-10-18 01:04:22 -0700

    Added endpoints to get all ebay item ids as well as specific item data. Need to add API endpoint to send POST methods to edit an ebay item in the database. Need to think about how user will filter items when searching

--- backend/inventory_management_system/routes.py
+++ backend/inventory_management_system/routes.py
@@ -1,27 +1,46 @@
 from flask import Flask, render_template, request, Blueprint, request, redirect, url_for, jsonify
-from .models import Item
+from .models import Item, Url
 from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def index():
-    # item = Item(382190381, 2014, "Toyota", "Prius", 8028, "B5")
-    # db.session.add(item)
-    # db.session.commit()
-    # print("Created and added item...")
-
-    print(Item.query.all())
-    print(db)
-
     return "Nyhallo world!"
 
 @main.route("/api/getAllItems")
 def getAllItems():
-    return jsonify({'id': [154423432,432565,4365,6376,4354234,124235,4324]})
+    # Returns the ids off all ebay items
+    items = Item.query.all()
+    return jsonify({'ids': [item.id for item in items]})
 
-@main.route("/api/getItem/<int:SKU>")
-def getItem(SKU):
-    # Use SKU from API endpoint, to know which ebay item information to grab from the database
-    # Code to grab ebay item from sku through sqlite database.
-    return jsonify({'SKU': SKU})
+@main.route("/api/getItem/<int:id>")
+def getItem(id):
+    # Use id from API endpoint, to know which ebay item information to grab from the database
+    # Returns all JSON item for given ebay item id.
+    item = Item.query.get_or_404(id)
+    image_urls = [image_url.image_url for image_url in item.image_url]
+
+    return jsonify({
+        'id': item.id,
+        'title': item.title,
+        'price': item.price,
+        'status': item.status,
+        'listed_date': item.listed_date,
+        'ebay_url': item.ebay_url,
+        'location': item.location,
+        'last_updated_date': item.last_updated_date,
+        'length': item.length,
+        'width': item.width,
+        'height': item.height,
+        'weight': item.weight,
+        'image_urls': image_urls,
+    })
+
+@main.route('/api/editItem/<int:id>')
+def editItem(id):
+    # TODO: Given the ebay item id as well as a JSON data payload through a POST method. Update the info in the database.
+    # The only info that SHOULD be updated are: location, last_updated_date, length, width, height, and weight.
+    # id, title, price, status, listed_date, and ebay_url SHOULD NOT BE UPDATED THROUGH FLASK, SHOULD BE PULLED FROM EBAY API.
+
+    return f"editing item: {id}"
