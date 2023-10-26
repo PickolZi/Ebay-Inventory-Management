be6a6a8109a1397c708b39ae2713a79d84e2dc98
John S <125219797+JohnSanXD@users.noreply.github.com>
2023-10-25 23:02:05 -0700

    Created React Frontend using Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from '../pages/home'
+
+function App() {
+  return <Home />
+}
+
+export default App
---
+++ frontend/utils/fetch-from-flask-api.js
@@ -0,0 +1,18 @@
+// When fetching data from our backend flask API, we have to specify the address each time and write duplicate code each time. 
+// Until we have a domain for the api, we will have to hardcode our machine's address in here.
+
+// const machineIP = "http://68.190.242.157";
+const machineIP = "http://localhost:5000";
+
+const fetchFromFlaskAPIUsingGet = async (endPoint) => {
+    // url: Flask API endpoint we're trying to get data from.
+    return await fetch(machineIP + endPoint, {method: "GET"})
+    .then((response) => {
+        return response.json();
+    })
+    .then((res) => {
+        return res;
+    })
+};
+
+export default fetchFromFlaskAPIUsingGet;
