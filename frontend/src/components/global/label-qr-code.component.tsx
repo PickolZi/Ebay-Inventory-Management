ea02fdb74deac6d93816fe2699bc73b5d644cfd2
PickolZi <pickol876@gmail.com>
2024-03-29 12:37:39 -0700

    Finally fixed domain to point to two different servers, one for the frontend, and using a subdomain to point to the backend. Changed MACHINE_IP variable to point endpoint to backend server

--- frontend/src/components/global/label-qr-code.component.tsx
+++ frontend/src/components/global/label-qr-code.component.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import QRCode from "react-qr-code";
 
-import { MACHINE_IP } from "@/utils/machine-ip";
+import { MACHINE_IP_HOME } from "@/utils/machine-ip";
 
 const LabelQRCode:React.FC<{itemID:number}> = ({itemID}) => {
-    const websiteURL = `${MACHINE_IP}/pages/items/${itemID}`
+    const websiteURL = `${MACHINE_IP_HOME}/pages/items/${itemID}`
 
     return (
         <QRCode value={websiteURL} style={{ height: "100px", width: "100px" }}/>
--- frontend/src/utils/machine-ip.js
+++ frontend/src/utils/machine-ip.js
@@ -1,2 +1,3 @@
-export const MACHINE_IP = "https://bassetinventory.com";
-// export const MACHINE_IP = "http://localhost";
+export const MACHINE_IP = "https://api.bassetinventory.com";  // API endpoint for the backend.
+export const MACHINE_IP_HOME = "https://bassetinventory.com";  // Home domain name for QR codes.
+// export const MACHINE_IP = "http://localhost";  // Testing running flask backend on local development.
